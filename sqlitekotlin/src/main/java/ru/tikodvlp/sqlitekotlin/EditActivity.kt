492e3feec6d0838c8ae9d499e26ff7b6d8a1c8a5
Tigran <tigranma@gmail.com>
2022-03-09 20:48:00 +0300

    SQLite database app in Kotlin

--- sqlitekotlin/src/main/java/ru/tikodvlp/sqlitekotlin/EditActivity.kt
+++ sqlitekotlin/src/main/java/ru/tikodvlp/sqlitekotlin/EditActivity.kt
@@ -2,10 +2,32 @@ package ru.tikodvlp.sqlitekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class EditActivity : AppCompatActivity() {
+
+    lateinit var mainImageLayout:ConstraintLayout
+    lateinit var fbAddImage:ImageButton
+    lateinit var imButtonDeleteImage:ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        mainImageLayout = findViewById(R.id.mainImageLayout)
+        fbAddImage = findViewById(R.id.fbAddImage)
+        imButtonDeleteImage = findViewById(R.id.imButtonDeleteImage)
+    }
+
+    fun onClickAddImage(view: View) {
+        mainImageLayout.visibility = View.VISIBLE
+        fbAddImage.visibility = View.GONE
+    }
+
+    fun onClickDeleteImage(view: View) {
+        mainImageLayout.visibility = View.GONE
+        fbAddImage.visibility = View.VISIBLE
     }
 }
