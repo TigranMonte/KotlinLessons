413604cbaae611f701cd329e9f723ee3aaa02215
Tigran <tigranma@gmail.com>
2022-12-29 16:20:42 +0300

    NyetHack (String format)

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -1,18 +1,18 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
-    var healthPoints = 89
-    val isBlessed = true
+    var HP = 100
+    val B = true
     val isImmortal = false
 
     // aura
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    println(auraColor)
+    val auraVisible = B && HP > 50 || isImmortal
+    val A = if (auraVisible) "GREEN" else "NONE"
 
-    val healthStatus = when (healthPoints) {
+
+    val H = when (HP) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
+        in 75..89 -> if (B) {
             "has some minor wounds but is healing quite quickly!"
         } else {
             "has some minor wounds."
@@ -20,5 +20,6 @@ fun main(args: Array<String>) {
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
-    println("$name $healthStatus")
+    val statusFormatString = "(HP: $HP) (Aura: $A) -> $name $H"
+    println(statusFormatString)
 }
