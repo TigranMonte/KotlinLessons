34cb56acd672283c57e9cc267e01d272b2a9f504
Tigran <tigranma@gmail.com>
2022-12-30 19:35:37 +0300

    NyetHack (Functions)

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -1,25 +1,47 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
-    var HP = 100
-    val B = true
+    var healthPoints = 100
+    val isBlessed = true
     val isImmortal = false
 
     // aura
-    val auraVisible = B && HP > 50 || isImmortal
-    val A = if (auraVisible) "GREEN" else "NONE"
+    val aura = auraColor(isBlessed, healthPoints, isImmortal)
 
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
-    val H = when (HP) {
+    printPlayerStatus(healthPoints, aura, isBlessed,name, healthStatus)
+
+    castFireball(5)
+}
+
+private fun printPlayerStatus(healthPoints: Int, aura: String, isBlessed: Boolean, name: String, healthStatus: String) {
+    val statusFormatString = "(HP: $healthPoints) (Aura: $aura) " +
+            "(Blessed: ${if (isBlessed) "YES" else "NO"}) -> $name $healthStatus"
+    println(statusFormatString)
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val aura = if (auraVisible) "GREEN" else "NONE"
+    return aura
+}
+
+private fun castFireball(numFireballs: Int = 2) {
+    println("A glass of Fireball springs into existence. (x$numFireballs)")
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
+    val healthStatus = when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
-        in 75..89 -> if (B) {
+        in 75..89 -> if (isBlessed) {
             "has some minor wounds but is healing quite quickly!"
         } else {
             "has some minor wounds."
         }
+
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
-    val statusFormatString = "(HP: $HP) (Aura: $A) -> $name $H"
-    println(statusFormatString)
+    return healthStatus
 }
