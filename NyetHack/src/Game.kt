85ce16b078ba3232f0e01e21a573a68e4a3c171d
Tigran <tigranma@gmail.com>
2023-01-04 19:39:30 +0300

    NyetHack (Functions)

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -12,16 +12,19 @@ fun main(args: Array<String>) {
 
     castFireball(5)
 }
+
 private fun printPlayerStatus(healthPoints: Int, aura: String, isBlessed: Boolean, name: String, healthStatus: String) {
     val statusFormatString = "(HP: $healthPoints) (Aura: $aura) " +
             "(Blessed: ${if (isBlessed) "YES" else "NO"}) -> $name $healthStatus"
     println(statusFormatString)
 }
+
 private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val aura = if (auraVisible) "GREEN" else "NONE"
     return aura
 }
+
 private fun castFireball(numFireballs: Int = 2) =
     println("A glass of Fireball springs into existence. (x$numFireballs)")
 
@@ -34,7 +37,6 @@ private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
         } else {
             "has some minor wounds."
         }
-
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
