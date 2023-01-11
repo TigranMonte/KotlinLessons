fc6ce7553abcb18d69474d0f6e9af64d36e5aa6b
Tigran <tigranma@gmail.com>
2023-01-11 17:38:05 +0300

    SimVillage Анонимные функции (ссылка на функцию влямбде)

--- NyetHack/src/SimVillage.kt
+++ NyetHack/src/SimVillage.kt
@@ -1,11 +1,17 @@
 fun main(args: Array<String>) {
-    runSimulation("Leon") { playerName, numBuildings ->
+    runSimulation("Leon", ::printConstructionCost) { playerName, numBuildings ->
         val currentYear = 2023
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
 }
-inline fun runSimulation(playerName: String, greetingFun: (String, Int) -> String) {
+inline fun runSimulation(playerName: String,costPrinter: (Int) -> Unit, greetingFun: (String, Int) -> String) {
     val numBuildings = (1..5).shuffled().last()
+    costPrinter(numBuildings)
     println(greetingFun(playerName, numBuildings))
+}
+
+fun printConstructionCost(numBuildings: Int) {
+    val cost = 500
+    println("construction cost: ${cost * numBuildings}")
 }
