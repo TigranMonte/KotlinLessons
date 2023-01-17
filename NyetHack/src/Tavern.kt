188ae345ce9b85e747fe52a57b520d0fae8a5503
Tigran <tigranma@gmail.com>
2023-01-17 17:45:45 +0300

    Tavern (Null-safety, !! оператор)

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,11 +1,14 @@
 fun main(args: Array<String>) {
-    var beverage = readLine()?.let{
-        if (it.isNotBlank()) {
-            it.capitalize()
-        } else {
-            "Hadiji Ale"
-        }
-    }
-//    beverage = null
+//    var beverage = readLine()?.let{
+//        if (it.isNotBlank()) {     безопасный вызов с функцией let
+//            it.capitalize()
+//        } else {
+//            "Hadiji Ale"
+//        }
+//    }
+
+    var beverage = readLine()!!.capitalize() // оператор контроля non-null вызывается когда точно
+                                            // известно, что никогда не получим null
+    //    beverage = null
     println(beverage)
 }
