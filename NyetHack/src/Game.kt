0b55e762633e8518c5160c63a3090adc978b88a8
Tigran <tigranma@gmail.com>
2023-02-02 17:47:51 +0300

    Class (свойства и инкапсуляция)

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -1,17 +1,18 @@
 fun main(args: Array<String>) {
-    val name = "Madrigal"
+
     var healthPoints = 100
     val isBlessed = true
     val isImmortal = false
+
+    val player = Player()
+    player.castFireball()
 
     // aura
     val aura = auraColor(isBlessed, healthPoints, isImmortal)
 
     val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
-    printPlayerStatus(healthPoints, aura, isBlessed,name, healthStatus)
-
-    castFireball(5)
+    printPlayerStatus(healthPoints, aura, isBlessed, player.name, healthStatus)
 }
 private fun printPlayerStatus(healthPoints: Int, aura: String, isBlessed: Boolean, name: String, healthStatus: String) {
     val statusFormatString = "(HP: $healthPoints) (Aura: $aura) " +
@@ -23,8 +24,6 @@ private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean
     val aura = if (auraVisible) "GREEN" else "NONE"
     return aura
 }
-private fun castFireball(numFireballs: Int = 2) =
-    println("A glass of Fireball springs into existence. (x$numFireballs)")
 private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
     when (healthPoints) {
         100 -> "is in excellent condition!"
---
+++ NyetHack/src/Player.kt
@@ -0,0 +1,6 @@
+class Player {
+
+    val name = "Madrigal"
+    fun castFireball(numFireballs: Int = 2) =
+        println("A glass of Fireball springs into existence. (x$numFireballs)")
+}
