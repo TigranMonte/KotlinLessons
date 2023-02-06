c13e3c8024a7d23b9acb3735419436894bb0d680
Tigran <tigranma@gmail.com>
2023-02-06 20:58:47 +0300

    Class (свойства, инкапсуляция и методы свойств)

--- NyetHack/src/Player.kt
+++ NyetHack/src/Player.kt
@@ -1,6 +1,11 @@
 class Player {
 
-    val name = "Madrigal"
+    var name = "madrigal"
+        // переопределение метода чтения
+        get() = field.capitalize()
+        private set(value) {
+            field = value.trim()
+        }
     fun castFireball(numFireballs: Int = 2) =
         println("A glass of Fireball springs into existence. (x$numFireballs)")
 }
