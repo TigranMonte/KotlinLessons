62c4d960e1a173ca593137849e29e48ada3ed012
Tigran <tigranma@gmail.com>
2023-02-28 15:37:42 +0300

    Объекты (enum class и перегрузка операторов)

--- NyetHack/src/nyethack/Navigation.kt
+++ NyetHack/src/nyethack/Navigation.kt
@@ -1,5 +1,16 @@
 package nyethack
+enum class Direction (private val coordinate: Coordinate) {
+    NORTH(Coordinate(0, -1)),
+    EAST(Coordinate(1, 0)),
+    SOUTH(Coordinate(0,1)),
+    West(Coordinate(-1, 0));
+
+    fun updateCoordinate(playerCoordinate: Coordinate) =
+        coordinate + playerCoordinate
+}
 
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
+
+    operator fun plus (other: Coordinate) = Coordinate(x + other.x, y + other.y)
 }
