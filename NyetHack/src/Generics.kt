08fa9b9160e2d2c19cbe722c874e3fb62e567ff3
Tigran <tigranma@gmail.com>
2023-03-11 15:34:21 +0300

    Generics (generic functions)

--- NyetHack/src/Generics.kt
+++ NyetHack/src/Generics.kt
@@ -1,5 +1,11 @@
+
 class LootBox<T> (item: T) {
+    var open = false
     private var loot: T = item
+
+    fun fetch(): T? {
+        return loot.takeIf { open }
+    }
 }
 
 class Fedora(val name: String, val value: Int)
@@ -7,6 +13,12 @@ class Fedora(val name: String, val value: Int)
 class Coin(val value: Int)
 
 fun main(args: Array<String>) {
+        // определения типов можно не указывать\\
     val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
+
+    lootBoxOne.open = true
+    lootBoxOne.fetch()?.run {
+        println("You retrieve $name from the box!")
+    }
 }
