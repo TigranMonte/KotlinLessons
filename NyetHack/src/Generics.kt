cfee0bf000abeefd1eda45f952c79958ca43f24d
Tigran <tigranma@gmail.com>
2023-03-14 17:20:40 +0300

    Generics (vararg)

--- NyetHack/src/Generics.kt
+++ NyetHack/src/Generics.kt
@@ -1,13 +1,13 @@
 
-class LootBox<T: Loot> (item: T) {
+class LootBox<T: Loot> (vararg item: T) {
     var open = false
-    private var loot: T = item
+    private var loot: Array<out T> = item
 
-    fun fetch(): T? {
-        return loot.takeIf { open }
+    fun fetch(item: Int): T? {
+        return loot[item].takeIf { open }
     }
-    fun <R> fetch(lootMoodFunction: (T) -> R): R? {
-        return lootMoodFunction(loot).takeIf { open }
+    fun <R> fetch(item: Int, lootMoodFunction: (T) -> R): R? {
+        return lootMoodFunction(loot[item]).takeIf { open }
     }
 }
 
@@ -19,14 +19,16 @@ class Coin(value: Int): Loot(value)
 
 fun main(args: Array<String>) {
         // определения типов можно не указывать\\
-    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15))
+    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15),
+        Fedora("a dazzling magenta fedora", 25))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
 
-    lootBoxOne.fetch()?.run {
+    lootBoxOne.open
+    lootBoxOne.fetch(1)?.run {
         println("You retrieve $name from the box!")
     }
 
-    val coin = lootBoxOne.fetch() {
+    val coin = lootBoxOne.fetch(0) {
         Coin(it.value * 3)
     }
     coin?.let { println(it.value) }
