5334eca570b7bc8dfc548e9e0549de54311b3c58
Tigran <tigranma@gmail.com>
2023-03-18 19:39:35 +0300

    Extensions (свойства-расширения)

--- NyetHack/src/Extensions.kt
+++ NyetHack/src/Extensions.kt
@@ -1,11 +1,15 @@
 fun main(args: Array<String>) {
     "Madrigal has left the building".easyPrint().addEnthusiasm().easyPrint()
     42.easyPrint()
+    "How many vowels?".numVowels.easyPrint()
 }
 
 fun <T> T.easyPrint(): T{
     println(this)
     return this
 }
+
+val String.numVowels
+    get() = count { "aeiouy".contains(it) }
 
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
