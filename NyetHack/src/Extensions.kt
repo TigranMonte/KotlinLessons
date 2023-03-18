0189abaae38bf0d4e316985c603dbf432455b834
Tigran <tigranma@gmail.com>
2023-03-18 18:33:01 +0300

    Extensions (расширение для суперкласса)

--- NyetHack/src/Extensions.kt
+++ NyetHack/src/Extensions.kt
@@ -1,5 +1,8 @@
 fun main(args: Array<String>) {
-    println("Madrigal has left the building".addEnthusiasm())
+    println("Madrigal has left the building".addEnthusiasm()).easyPrint()
+    42.easyPrint()
 }
+
+fun Any.easyPrint() = println(this)
 
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
