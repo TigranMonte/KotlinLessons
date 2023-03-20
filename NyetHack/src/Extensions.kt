606c83ff8b623d7a8cdb0d80404bb0001e9d74a7
Tigran <tigranma@gmail.com>
2023-03-20 21:31:02 +0300

    Extensions (расширения для типов с поддержкой null)

--- NyetHack/src/Extensions.kt
+++ NyetHack/src/Extensions.kt
@@ -2,6 +2,9 @@ fun main(args: Array<String>) {
     "Madrigal has left the building".easyPrint().addEnthusiasm().easyPrint()
     42.easyPrint()
     "How many vowels?".numVowels.easyPrint()
+
+    val nullableString: String? = null
+    nullableString printWithDefault "Default string"
 }
 
 fun <T> T.easyPrint(): T{
@@ -13,3 +16,5 @@ val String.numVowels
     get() = count { "aeiouy".contains(it) }
 
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
+
+infix fun String?.printWithDefault(default: String) = print(this ?:default)
