6ce30c80020f554427367bf56e037d55fefcb4d3
Tigran <tigranma@gmail.com>
2023-04-03 21:45:17 +0300

    Interop (Java/Kotlin) - аннотаци

--- NyetHack/src/Interop/Hero.kt
+++ NyetHack/src/Interop/Hero.kt
@@ -1,5 +1,4 @@
 package Interop
-
 fun main(args: Array<String>) {
 
     val adversary = Jhava()
@@ -14,4 +13,25 @@ fun main(args: Array<String>) {
 
     adversary.greeting = "Hello, Hero."
     println(adversary.utterGreeting())
+
+    adversary.offerFood()
+}
+
+fun makeProclamation() = "Greetings, beast!"
+@JvmOverloads
+fun handOverFood(leftHand: String = "berries", rightHand: String = "beef") {
+    println("Mmmmm... you hand over some delicious $leftHand and $rightHand")
+}
+
+class SpellBook {
+    @JvmField
+    val spells = listOf("Magic Ms. L", "Lay on Hans")
+
+    companion object {
+        @JvmField
+        val MAX_SPELL_COUNT = 10
+
+        @JvmStatic
+        fun getSpellbookGreeting() = println("I am the Great Gilmore!")
+    }
 }
--- NyetHack/src/Interop/Jhava.java
+++ NyetHack/src/Interop/Jhava.java
@@ -7,6 +7,17 @@ public class Jhava {
 
     private int hitPoints = 52489112;
     private String greeting = "BLARGH";
+
+    public static void main(String[] args) {
+        System.out.println(HeroKt.makeProclamation());
+        System.out.println("Spells:");
+        SpellBook spellBook = new SpellBook();
+        for (String spell : spellBook.spells) {
+            System.out.println(spell);
+        }
+        System.out.println("Max spell count: " + SpellBook.MAX_SPELL_COUNT);
+        SpellBook.getSpellbookGreeting();
+    }
     @NotNull
     public String utterGreeting() {
         return greeting;
@@ -26,5 +37,9 @@ public class Jhava {
 
     public void setGreeting(String greeting) {
         this.greeting = greeting;
+    }
+
+    public void offerFood() {
+        HeroKt.handOverFood("pizza");
     }
 }
